3c0bac9183a32e737a8c344809e11abca0b834cd
Serega Gorbunov <gorbunov.sergei0808@gmail.com>
2026-05-20 12:54:43 +0300

    var28

--- var28/15.py
+++ var28/15.py
@@ -4,7 +4,7 @@ def Del(n, m):
 for a in range(1, 10000):
     k = 1
     for x in range(1, 10000):
-        f = (not Del(x, A)) <= (Del(x, 48) <= (not Del(x, 173)))
+        f = (not Del(x, a)) <= (Del(x, 48) <= (not Del(x, 173)))
         if not f:
             k = 0
             break
